88b642c668861e531ef6f9adf605319b8ab00196
Sergey Ostrovskiy <s.ostrovskiy@4payments.com>
2023-11-06 02:29:18 +0300

    added tests

---
+++ internal/client/infrastrucrure/filemanager/userauth_test.go
@@ -0,0 +1,111 @@
+package filemanager
+
+import (
+	"github.com/kripsy/GophKeeper/internal/models"
+	"os"
+	"reflect"
+	"testing"
+)
+
+const (
+	Login    = "testUser"
+	Password = "testPassword"
+)
+
+func Test_userAuth_GetUser(t *testing.T) {
+	defer os.RemoveAll(storageDir)
+	auth, err := NewUserAuth(storageDir)
+	if err != nil {
+		t.Fatalf("Failed to create NewUserAuth: %v", err)
+	}
+	tests := []struct {
+		name        string
+		auth        Auth
+		user        *models.User
+		prepareFunc func()
+		want        models.UserMeta
+		wantErr     bool
+	}{
+		{
+			name: "ok",
+			auth: auth,
+			prepareFunc: func() {
+				if _, err = auth.CreateUser(&models.User{Username: Login, Password: Password}, true); err != nil {
+					t.Fatalf("Failed prepare user: %v", err)
+				}
+			},
+			user:    &models.User{Username: Login, Password: Password},
+			want:    models.UserMeta{Username: Login, IsLocalStorage: true, Data: make(models.MetaData)},
+			wantErr: false,
+		},
+		{
+			name:        "failed not created user",
+			auth:        auth,
+			prepareFunc: func() {},
+			user:        &models.User{Username: "notCreatedUser", Password: Password},
+			wantErr:     true,
+		},
+		{
+			name: "failed invalid password",
+			auth: auth,
+			prepareFunc: func() {
+				if _, err = auth.CreateUser(&models.User{Username: Login, Password: Password}, true); err != nil {
+					t.Fatalf("Failed prepare user: %v", err)
+				}
+			},
+			user:    &models.User{Username: Login, Password: "invalidPassword"},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.prepareFunc()
+
+			got, err := tt.auth.GetUser(tt.user)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetUser() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetUser() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_userAuth_CreateUser(t *testing.T) {
+	defer os.RemoveAll(storageDir)
+	auth, err := NewUserAuth(storageDir)
+	if err != nil {
+		t.Fatalf("Failed to create NewUserAuth: %v", err)
+	}
+	tests := []struct {
+		name           string
+		auth           Auth
+		user           *models.User
+		want           models.UserMeta
+		isLocalStorage bool
+		wantErr        bool
+	}{
+		{
+			name:           "ok",
+			auth:           auth,
+			user:           &models.User{Username: Login, Password: Password},
+			want:           models.UserMeta{Username: Login, IsLocalStorage: true, Data: make(models.MetaData)},
+			isLocalStorage: true,
+			wantErr:        false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.auth.CreateUser(tt.user, tt.isLocalStorage)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CreateUser() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CreateUser() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
