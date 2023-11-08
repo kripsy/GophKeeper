aed4ac69a276e935094210cf6492374342adba17
evpost <kripsy93@yandex.ru>
2023-11-08 11:18:48 +0300

    add middleware unit test

--- internal/server/controller/middleware_grpc_controller_test.go
+++ internal/server/controller/middleware_grpc_controller_test.go
@@ -1,11 +1,20 @@
 package controller_test
 
 import (
+	"context"
 	"testing"
+	"time"
 
+	"github.com/golang/mock/gomock"
 	"github.com/kripsy/GophKeeper/internal/server/controller"
+	"github.com/kripsy/GophKeeper/internal/server/controller/mocks"
+	"github.com/kripsy/GophKeeper/internal/utils"
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 )
 
 func TestInitMyMiddleware(t *testing.T) {
@@ -31,6 +40,226 @@ func TestInitMyMiddleware(t *testing.T) {
 			// Проверяем, что повторный вызов возвращает тот же экземпляр
 			sameMiddleware := controller.InitMyMiddleware(mockLogger, "newSecret")
 			assert.Equal(t, middleware, sameMiddleware)
+		})
+	}
+}
+
+func TestAuthInterceptor(t *testing.T) {
+	const (
+		loginMethod    = "/pkg.api.gophkeeper.v1.GophKeeperService/Login"
+		registerMethod = "/pkg.api.gophkeeper.v1.GophKeeperService/Register"
+		pingMethod     = "/pkg.api.gophkeeper.v1.GophKeeperService/Ping"
+	)
+	l := zap.NewNop()
+	secret := "secret"
+
+	middleware := controller.InitMyMiddleware(l, secret)
+
+	userID := 1
+	userName := "testuser"
+	validToken, err := utils.BuildJWTString(userID, userName, secret, time.Hour)
+	assert.NoError(t, err)
+
+	invalidToken, err := utils.BuildJWTString(userID, userName, secret+"fake", time.Hour)
+	assert.NoError(t, err)
+
+	tests := []struct {
+		name        string
+		fullMethod  string
+		token       string
+		secret      string
+		wantErr     bool
+		isProtected bool
+	}{
+		{
+			name:        "ValidToken",
+			fullMethod:  "/pkg.api.gophkeeper.v1.GophKeeperService/ValidMethod",
+			token:       validToken,
+			secret:      secret,
+			wantErr:     false,
+			isProtected: true,
+		},
+		{
+			name:        "InvalidToken",
+			fullMethod:  "/pkg.api.gophkeeper.v1.GophKeeperService/ValidMethod",
+			token:       invalidToken,
+			secret:      secret,
+			wantErr:     true,
+			isProtected: true,
+		},
+		{
+			name:        "NoMetadata",
+			fullMethod:  "/pkg.api.gophkeeper.v1.GophKeeperService/ValidMethod",
+			token:       "",
+			secret:      secret,
+			wantErr:     true,
+			isProtected: true,
+		},
+		{
+			name:        "NoAuthHeader",
+			fullMethod:  "/pkg.api.gophkeeper.v1.GophKeeperService/ValidMethod",
+			token:       "",
+			secret:      secret,
+			wantErr:     true,
+			isProtected: true,
+		},
+		{
+			name:        "EmptyToken",
+			fullMethod:  "/pkg.api.gophkeeper.v1.GophKeeperService/ValidMethod",
+			token:       "Bearer ",
+			secret:      secret,
+			wantErr:     true,
+			isProtected: true,
+		},
+		{
+			name:        "UnprotectedMethod",
+			fullMethod:  loginMethod,
+			token:       validToken,
+			secret:      secret,
+			wantErr:     false,
+			isProtected: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			testHandler := func(ctx context.Context, req interface{}) (interface{}, error) {
+				if tt.isProtected {
+					if ctx.Value(utils.USERNAMECONTEXTKEY) != userName {
+						t.Errorf("Username in context is incorrect")
+					}
+					if ctx.Value(utils.USERIDCONTEXTKEY) != userID {
+						t.Errorf("UserID in context is incorrect")
+					}
+				}
+
+				return "testResponse", nil
+			}
+
+			md := metadata.New(map[string]string{
+				utils.AUTHORIZATIONHEADER: tt.token,
+			})
+			ctx := metadata.NewIncomingContext(context.Background(), md)
+			switch tt.name {
+			case "NoMetadata":
+				ctx = context.Background()
+			case "NoAuthHeader":
+				md := metadata.New(map[string]string{})
+				ctx = metadata.NewIncomingContext(context.Background(), md)
+			case "EmptyToken":
+				md := metadata.New(map[string]string{
+					utils.AUTHORIZATIONHEADER: "Bearer ",
+				})
+				ctx = metadata.NewIncomingContext(context.Background(), md)
+			}
+
+			info := &grpc.UnaryServerInfo{
+				FullMethod: tt.fullMethod,
+			}
+
+			_, err := middleware.AuthInterceptor(ctx, nil, info, testHandler)
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf("AuthInterceptor() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestStreamAuthInterceptor(t *testing.T) {
+	secret := "secret"
+	logger := zap.NewNop()
+	middleware := controller.InitMyMiddleware(logger, secret)
+	userID := 1
+	userName := "testuser"
+	validToken, _ := utils.BuildJWTString(userID, userName, secret, time.Hour)
+	invalidToken, _ := utils.BuildJWTString(userID, userName, "fake", time.Hour)
+
+	tests := []struct {
+		name       string
+		fullMethod string
+		token      string
+		wantErr    bool
+		wantCode   codes.Code
+		setup      func(*mocks.MockServerStream)
+	}{
+		{
+			name:       "ValidToken",
+			fullMethod: "/package.Service/StreamMethod",
+			token:      validToken,
+			wantErr:    false,
+			setup: func(mockStream *mocks.MockServerStream) {
+				md := metadata.New(map[string]string{
+					utils.AUTHORIZATIONHEADER: utils.TOKENPREFIX + validToken,
+				})
+				ctx := metadata.NewIncomingContext(context.Background(), md)
+				mockStream.EXPECT().Context().Return(ctx).AnyTimes()
+			},
+		},
+		{
+			name:       "InvalidToken",
+			fullMethod: "/package.Service/StreamMethod",
+			token:      invalidToken,
+			wantErr:    true,
+			wantCode:   codes.Unauthenticated,
+			setup: func(mockStream *mocks.MockServerStream) {
+				md := metadata.New(map[string]string{
+					utils.AUTHORIZATIONHEADER: utils.TOKENPREFIX + invalidToken,
+				})
+				ctx := metadata.NewIncomingContext(context.Background(), md)
+				mockStream.EXPECT().Context().Return(ctx).AnyTimes()
+			},
+		},
+		{
+			name:       "MetadataNotFound",
+			fullMethod: "/package.Service/StreamMethod",
+			token:      "",
+			wantErr:    true,
+			wantCode:   codes.Internal,
+			setup: func(mockStream *mocks.MockServerStream) {
+				ctx := context.Background()
+				mockStream.EXPECT().Context().Return(ctx).AnyTimes()
+			},
+		},
+		{
+			name:       "AuthorizationHeaderMissing",
+			fullMethod: "/package.Service/StreamMethod",
+			token:      "",
+			wantErr:    true,
+			setup: func(mockStream *mocks.MockServerStream) {
+				md := metadata.New(map[string]string{})
+				ctx := metadata.NewIncomingContext(context.Background(), md)
+				mockStream.EXPECT().Context().Return(ctx).AnyTimes()
+			},
+			wantCode: codes.Unauthenticated,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			mockStream := mocks.NewMockServerStream(ctrl)
+			tt.setup(mockStream)
+
+			info := &grpc.StreamServerInfo{
+				FullMethod: tt.fullMethod,
+			}
+
+			err := middleware.StreamAuthInterceptor(nil, mockStream,
+				info, func(srv interface{}, stream grpc.ServerStream) error {
+					return nil
+				})
+
+			if tt.wantErr {
+				assert.Error(t, err)
+				if err != nil {
+					assert.Equal(t, tt.wantCode, status.Code(err))
+				}
+			} else {
+				assert.NoError(t, err)
+			}
 		})
 	}
 }
